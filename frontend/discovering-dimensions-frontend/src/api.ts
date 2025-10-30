65c40634abd47e089a3e8735256bcd30b3f4036a
hapaxlegomena <thesemariofilms@gmail.com>
2025-10-30 06:50:40 +0000

    Remove library causing error

--- frontend/discovering-dimensions-frontend/src/api.ts
+++ frontend/discovering-dimensions-frontend/src/api.ts
@@ -1,14 +1,15 @@
-import axios from "axios";
-import * as Network from "expo-network"
+import axios from 'axios';
 
 // DEV TIP: set this per environment
 // - iOS Simulator -> http://localhost:8000
 // - Android Emulator -> http://10.0.2.2:8000
 // - Physical device -> http://<your-computer-LAN-IP>:8000
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL || `http://${await Network.getIpAddressAsync()}:8000`;
+const BASE_URL =
+  process.env.EXPO_PUBLIC_API_URL || `http://your-ip-address:8000`;
 
-
-export const api = axios.create({
+const api = axios.create({
   baseURL: BASE_URL,
   timeout: 8000,
 });
+
+export default api;
