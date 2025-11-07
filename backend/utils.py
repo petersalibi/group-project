961b475e96e7e8da7ce1923962ec99d246b61fe9
Iwan Sherlock <iwan.sherlock@warwick.ac.uk>
2025-11-07 11:27:34 +0000

    as before

--- backend/utils.py
+++ backend/utils.py
@@ -61,8 +61,10 @@ def parse_minimiser_params(params: dict):
     try:
         network_params = NetworkParams(**params.get("network", {}))
         data_type = TrainingDataType[params.get("data", "SINREGRESSION")]
+
         x_direction = torch.tensor(params.get("x_direction", torch.randn(100).tolist()))
         y_direction = torch.tensor(params.get("y_direction", torch.randn(100).tolist()))
+
         theta_0 = torch.tensor(params.get("theta_0", torch.randn(100).tolist()))
         optimiser = parse_optimiser(params.get("optimiser", "Adam"))
         learning_rate = params.get("learning_rate", 0.1)
