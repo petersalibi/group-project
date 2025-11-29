b9e260d8e132f48b957a8924c883f1c3ca299489
gamma82 <davidlitchfield.work@gmail.com>
2025-11-29 16:54:17 +0000

    Applied the correction - get_trajectories now takes in a model as input, performs the training then returns the model to its original state at the end

--- backend/directions.py
+++ backend/directions.py
@@ -159,7 +159,7 @@ def set_params_from_vector(model, vector):
         pointer += numel
         
 
-def get_trajectories(df : pd.DataFrame, independents : list[str], dependent : str, epochs : int = 200,
+def get_trajectories(df : pd.DataFrame, independents : list[str], dependent : str, model, epochs : int = 200,
                      record_interval : int = 1, n_random_samples : int = 500, sigma : float = 0.02) :
     
     all_vars = independents + [dependent]
@@ -176,15 +176,11 @@ def get_trajectories(df : pd.DataFrame, independents : list[str], dependent : st
 
     # Initialising everything
     
-    # Input-output shape of the network
-    n_inputs = len(independents)
-    n_outputs = train[dependent].nunique()
+    # Input-output shape of the network (REDUNDANT due to existing model)
+    # n_inputs = len(independents)
+    # n_outputs = train[dependent].nunique()
     
-    model = nn.Sequential(nn.Linear(n_inputs,5),
-                    nn.Tanh(),
-                    nn.Linear(5,5),
-                    nn.Tanh(),
-                    nn.Linear(5,n_outputs))
+    original_model_state = model.state_dict()
     
     lf = nn.CrossEntropyLoss()
     optim = torch.optim.Adam(model.parameters(), lr = 0.01)
@@ -255,6 +251,9 @@ def get_trajectories(df : pd.DataFrame, independents : list[str], dependent : st
                 loss = lf(model(X_train), Y_train )
                 
                 loss_vals.append(loss.item())
+
+    # Return to the original model state
+    model.load_state_dict(original_model_state)
 
     # Combine to get a tuple of the vectors and associated loss
     return parameter_snapshots, loss_vals
