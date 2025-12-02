fe75f5439f7f92bf778f6dc857ae8c97c65457fa
hapaxlegomena <thesemariofilms@gmail.com>
2025-12-02 01:08:49 +0000

    Add curric intro

---
+++ frontend/discovering-dimensions-frontend/app/(tabs)/curriculum/index.tsx
@@ -0,0 +1,75 @@
+import { UnorderedList } from '@/components/text-list';
+import { ThemedText } from '@/components/themed-text';
+import { Text } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function CurriculumIndex() {
+  return (
+    <Text>
+      <ThemedText type='title'>Introduction</ThemedText>
+      {'\n'}
+      {'\n'}
+      <ThemedText type='text'>
+        The Discovering Dimensions Loss Landscape tool is designed to help you
+        understand more about the topic surrounding loss landscapes, how
+        changing variables impacts the loss landscapes and visualise how
+        different common minimiser methods look on a loss landscape.
+        {'\n'}
+        {'\n'}
+        To aid in using this tool, we have created a curriculum to help you
+        develop a deeper understanding of neural networks so that you are able
+        to gather insightful information from this tool, whether its for your
+        studies or to help with a personal project or research.
+        {'\n'}
+        {'\n'}
+        This tool is aimed at people with basic knowledge of machine learning
+        and neural networks, with this course introducing loss and how it
+        relates to a neural network&apos;s performance, all the way to
+        understanding how to fine tune parameters, interpret different methods
+        and discover interesting comparisons between loss landscapes.
+        {'\n'}
+        {'\n'}
+        This course is split into three stages, so depending on your current
+        knowledge, you are able to start from wherever you like, using these
+        lessons as a guide to get the most out of using and interpreting this
+        tool.
+        {'\n'}
+        <UnorderedList>
+          <ThemedText type='text'>
+            <ThemedText type='textBold'>Stage 1:</ThemedText>{' '}
+            <Link href='/curriculum/stage-1'>
+              <ThemedText type='link'>Introducing loss</ThemedText>.
+            </Link>{' '}
+            Introducing a basic toy neural network, understanding the impact of
+            weight changes, introducing loss and how it impacts the prediction
+            of a network.
+          </ThemedText>
+          <ThemedText type='text'>
+            <ThemedText type='textBold'>Stage 2:</ThemedText>{' '}
+            <Link href='/curriculum/stage-2'>
+              <ThemedText type='link'>Features of loss landscapes</ThemedText>
+            </Link>
+            . Introducing loss landscapes, expanding our toy network to generate
+            a more interesting landscape and how different network architectures
+            impact the look of the loss landscape.
+          </ThemedText>
+          <ThemedText type='text'>
+            <ThemedText type='textBold'>Stage 3:</ThemedText>{' '}
+            <Link href='/curriculum/stage-3'>
+              <ThemedText type='link'>
+                Advanced loss landscape techniques
+              </ThemedText>
+            </Link>
+            . Demonstrating a broader range of toy networks, the impact of
+            hyperparameters and loss functions and how to compare networks to
+            each other.
+          </ThemedText>
+        </UnorderedList>
+        {'\n'}
+        {'\n'}
+        If you need any further help navigating the tool, please refer to our
+        detailed help guide.
+      </ThemedText>
+    </Text>
+  );
+}
