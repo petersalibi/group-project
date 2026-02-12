7d43437468a24fdd9c002f688ae06083a52f9b81
Iwan <iwan.sherlock@gmail.com>
2026-02-12 12:36:20 +0000

    Fixed merge issue

--- backend/losslandscape.py
+++ backend/losslandscape.py
@@ -62,7 +62,7 @@ def generate_loss_landscape(landscape_params: LandscapeParams, verbose=False):
             "y_direction": flatten_params(dir2).tolist(),
             "theta_0": flatten_params(model.parameters()).tolist(),
             "pca_trajectories": pca_trajectories,
-            "pca_mean" : pca_mean}
+            "pca_mean" : pca_mean,
             "column_labels": data.column_labels}
 
 def compute_loss_surface(model, X, y, dir1, dir2, loss, samples=200, scale=10, verbose=False, pca_mean = None):
