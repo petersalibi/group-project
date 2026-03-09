3f1e2f7fb6d5662be3e8586c8d7d1127dc843900
hapaxlegomena <thesemariofilms@gmail.com>
2026-03-09 14:36:00 +0000

    Remove useless imports

--- frontend/hooks/loss-landscape.ts
+++ frontend/hooks/loss-landscape.ts
@@ -1,5 +1,3 @@
-import { useEffect, useRef, useState, useCallback } from 'react';
-import { View } from 'react-native';
 import { useVisualisation } from './visualisation';
 import { useLandscapeControls } from './landscape-controls';
 import { PathConfigInterface } from '../components/path-config';
